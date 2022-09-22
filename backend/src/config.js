83ecf421b3ff00afaa5bb909b3fdceff6e0bcad2
Olivier <oli7931@gmail.com>
2022-09-21 21:04:00 -0400

    removed web

--- backend/src/config.js
+++ backend/src/config.js
@@ -36,7 +36,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://personalteddies.netlify.app/", // Replace with your website or remove this line if you do not have one.
+  external_url: "", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
